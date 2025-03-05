cf0c601236dd3e9cc4ec41aedd96188d6be12c82
Raphael Wieland <raphael@wielandtech.com>
2025-03-05 11:23:02 -0500

    add 'published' model manager

--- wielandtech/blog/models.py
+++ wielandtech/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -21,11 +26,11 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
-
         ordering = ('-publish',)
 
     def __str__(self):
-
         return self.title
