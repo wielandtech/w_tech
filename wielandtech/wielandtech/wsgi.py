c3d1be430d4045d5ccbbb7b897651f817648c7f2
w_tech <69324304+wielandtech@users.noreply.github.com>
2025-03-05 20:29:03 -0500

    Merge pull request #1
    * add dev container
    
    * install django, create wielandtech project
    
    * create blog app
    
    * create blog model
    
    * edit blog admin panel
    
    * add 'published' model manager
    
    * add blog Post model
    
    * add blog Post view
    
    * add blog app to project
    
    * add blog templates
    
    * add pagination
    
    * refactor to use class based view
    
    * add post_share feature to blog
    
    * fix bug
    
    * implement comment system
    
    * implement tagging system
    
    * implement tagging system
    
    * add similar posts by tag

---
+++ wielandtech/wielandtech/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wielandtech project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wielandtech.settings')
+
+application = get_wsgi_application()
