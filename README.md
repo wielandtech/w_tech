6b69b92c382af5423ddd84ac15c2480828ef3950
Raphael Wieland <raphael@wielandtech.com>
2025-03-05 19:18:32 -0500

    add post_share feature to blog

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # w_blog
 A Python blog web-app written in Django
+
+Requires the following set in a root level .env file:
+- DJANGO_SECRET_KEY
+- EMAIL_PASSWORD
--- wielandtech/wielandtech/settings.py
+++ wielandtech/wielandtech/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2zz13zm3@=ol_#h8yb3ak5*(&xb!(o$p%b65a*17qvn0wj&^e'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! TODO: turn off
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -122,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'mail.wielandtech.com'
+EMAIL_HOST_USER = 'no-reply@wielandtech.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
