afd9f1b80f0db5b0e78e95dce55de9c6dc8dfd3f
Raphael Wieland <raphael@wielandtech.com>
2025-03-05 20:25:37 -0500

    add similar posts by tag

--- wielandtech/blog/views.py
+++ wielandtech/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage,\
  PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
@@ -54,8 +55,13 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html',
-                  {'comments': comments, 'comment_form': comment_form, 'new_comment': new_comment, 'post': post})
+                  {'comments': comments, 'comment_form': comment_form, 'new_comment': new_comment, 'post': post,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
