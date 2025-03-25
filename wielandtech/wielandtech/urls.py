077d31042cecb44bd955f2f00e2704df7477459d
Raphael Wieland <raphael@wielandtech.com>
2025-03-24 22:18:16 -0400

    change static file location

--- wielandtech/wielandtech/urls.py
+++ wielandtech/wielandtech/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
 from blog.sitemaps import PostSitemap
+
+from wielandtech.wielandtech import settings
 
 sitemaps = {
  'posts': PostSitemap,
@@ -29,3 +32,5 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
