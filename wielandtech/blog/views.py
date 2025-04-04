c2cb4c4635eb6b4588a7bef3748ff2fb2119fc94
Raphael Wieland <raphael@wielandtech.com>
2025-04-04 01:11:11 -0400

    add search vector

--- wielandtech/blog/views.py
+++ wielandtech/blog/views.py
@@ -1,6 +1,5 @@
-from django.contrib.postgres.search import SearchVector
-from django.core.paginator import Paginator, EmptyPage,\
- PageNotAnInteger
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
@@ -73,9 +72,12 @@ def post_search(request):
          form = SearchForm(request.GET)
          if form.is_valid():
              query = form.cleaned_data['query']
+             search_vector = SearchVector('title', 'body')
+             search_query = SearchQuery(query)
              results = Post.published.annotate(
-                 search=SearchVector('title', 'body'),
-             ).filter(search=query)
+                 search=search_vector,
+                 rank=SearchRank(search_vector, search_query)
+             ).filter(search=search_query).order_by('-rank')
      return render(
          request,
          'blog/post/search.html',
