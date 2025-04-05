c132451ff9f341643c191489914c01c3b0e26b33
Raphael Wieland <raphael@wielandtech.com>
2025-04-05 00:48:00 -0400

    add password reset views and templates

--- wielandtech/account/urls.py
+++ wielandtech/account/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
