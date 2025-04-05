40b2edcda633fc934511ef088ba02786293fcccb
Raphael Wieland <raphael@wielandtech.com>
2025-04-05 15:37:04 -0400

    add profiles

--- wielandtech/account/forms.py
+++ wielandtech/account/forms.py
@@ -1,10 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
 
 
 class UserRegistrationForm(forms.ModelForm):
--- wielandtech/account/urls.py
+++ wielandtech/account/urls.py
@@ -6,5 +6,6 @@ app_name = 'account'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('edit/', views.edit, name='edit'),
     path('register/', views.register, name='register'),
 ]
