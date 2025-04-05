fc814a2aa22103e108f441270c71c8e91363a5a4
Raphael Wieland <raphael@wielandtech.com>
2025-04-05 01:26:32 -0400

    refactor some stuff

--- wielandtech/wielandtech/urls.py
+++ wielandtech/wielandtech/urls.py
@@ -28,6 +28,7 @@ sitemaps = {
 
 urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('blog/', include('blog.urls', namespace='blog')),
