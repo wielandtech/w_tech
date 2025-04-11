b2267153c40d70e53196b830a122e3d388f71ac8
Raphael Wieland <raphael@wielandtech.com>
2025-04-11 00:21:03 -0400

    add bookmarker

--- wielandtech/images/templates/bookmarklet_ launcher.js
+++ wielandtech/images/templates/bookmarklet_ launcher.js
@@ -4,7 +4,7 @@
     }
     else {
         document.body.appendChild(document.createElement('script')).
-            src='https:/www.wielandtech.com/static/js/bookmarklet.js?r='+Math.
+            src='https://www.wielandtech.com/static/js/bookmarklet.js?r='+Math.
         floor(Math.random()*99999999999999999999);
     }
 })();
