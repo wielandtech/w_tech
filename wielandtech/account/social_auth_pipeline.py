80e395123856cff4cadad693bd37449b5e9b0ded
Raphael Wieland <raphael@wielandtech.com>
2025-04-10 23:50:13 -0400

    add auth pipeline

--- wielandtech/account/social_auth_pipeline.py
+++ wielandtech/account/social_auth_pipeline.py
@@ -1,5 +1,4 @@
 from social_core.pipeline.user import create_user
-from social_core.pipeline.social_auth import create_social_auth
 from .models import Profile
 
 def create_profile(strategy, details, user=None, *args, **kwargs):
