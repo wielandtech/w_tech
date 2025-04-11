c6c205756e551862fb72f73742fd5af9fd3da493
Raphael Wieland <raphael@wielandtech.com>
2025-04-11 13:28:05 -0400

    add follow functionality

--- wielandtech/account/views.py
+++ wielandtech/account/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
-from .models import Profile
+from .models import Contact, Profile
+from common.decorators import ajax_required
 
 @login_required
 def user_list(request):
@@ -100,3 +102,24 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+                return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
