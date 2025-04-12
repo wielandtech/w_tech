7937ea74cee3f350f3a0499f6c58474998ab4bff
Raphael Wieland <raphael@wielandtech.com>
2025-04-11 21:42:16 -0400

    fix redis

--- wielandtech/wielandtech/settings.py
+++ wielandtech/wielandtech/settings.py
@@ -201,22 +201,3 @@ REDIS_HOST = os.getenv("REDIS_IP")
 REDIS_KEY = os.getenv("REDIS_KEY")
 REDIS_PORT = 6379
 REDIS_DB = 0
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"redis://:{REDIS_KEY}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "SOCKET_CONNECT_TIMEOUT": 5,
-            "SOCKET_TIMEOUT": 5,
-            "CONNECTION_POOL_CLASS": "redis.BlockingConnectionPool",
-            "CONNECTION_POOL_CLASS_KWARGS": {"max_connections": 50, "timeout": 20},
-            "MAX_CONNECTIONS": 1000,
-            "RETRY_ON_TIMEOUT": True,
-        }
-    }
-}
-
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
