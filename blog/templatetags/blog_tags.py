27fae262033b1a185faccc24a35867def001df1b
Raphael Wieland <raphael@wielandtech.com>
2025-04-12 23:10:29 -0400

    mess everything up

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -19,7 +19,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
 
-@register.inclusion_tag('blog/post/../templates/blog/post/latest_posts.html')
+@register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
