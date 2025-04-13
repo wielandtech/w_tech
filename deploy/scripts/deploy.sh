75472c0827726ec2961ab159972378f1e43205b8
Raphael Wieland <raphael@wielandtech.com>
2025-04-12 23:28:14 -0400

    mess everything up

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -5,5 +5,6 @@ git pull origin development
 docker compose -f docker-compose.yml build
 docker compose -f docker-compose.yml up -d
 
+docker compose -f docker-compose.yml exec web python manage.py makemigrations --noinput
 docker compose -f docker-compose.yml exec web python manage.py migrate --noinput
 docker compose -f docker-compose.yml exec web python manage.py collectstatic --noinput
