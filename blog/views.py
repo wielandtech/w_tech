c4a58a57b8005101f9c7b8932065bc65c079d413
Raphael Wieland <raphael@wielandtech.com>
2025-04-14 18:57:55 -0400

    make blog go dark

--- blog/views.py
+++ blog/views.py
@@ -61,8 +61,11 @@ def post_detail(request, year, month, day, post):
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
-                  {'comments': comments, 'comment_form': comment_form, 'new_comment': new_comment, 'post': post,
-                   'similar_posts': similar_posts, 'section': 'blog'})
+                  {'post': post,
+                   'comments': comments,
+                   'form': comment_form,
+                   'similar_posts': similar_posts,
+                   'section': 'blog'})
 
 
 def post_search(request):
