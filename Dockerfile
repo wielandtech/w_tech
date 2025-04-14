4cc4bd4b082371e00db34bec6978424de1e301c4
Raphael Wieland <raphael@wielandtech.com>
2025-04-13 21:54:45 -0400

    test noon

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ RUN apt-get update && apt-get install -y \
     libpq-dev \
     && rm -rf /var/lib/apt/lists/*
 
-
 # Install Python dependencies
 COPY requirements.txt .
 RUN pip install --upgrade pip && pip install -r requirements.txt
--- wielandtech/settings.py
+++ wielandtech/settings.py
@@ -19,10 +19,13 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "wielandtech.com",
     "www.wielandtech.com",
-    "159.89.246.9",
-    "174.138.92.200",
     "127.0.0.1",
     "localhost",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://www.wielandtech.com",
+    "https://wielandtech.com",
 ]
 
 SITE_ID = 1
@@ -172,8 +175,16 @@ REDIS_HOST = os.getenv("REDIS_IP", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 
+CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_REFERRER_POLICY = "same-origin"
+X_FRAME_OPTIONS = "DENY"
 
 # File upload settings
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
