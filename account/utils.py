734c5a777f3b07a8df9cd507079ffa71b1ed4742
Raphael Wieland <raphael@wielandtech.com>
2025-04-21 14:20:53 -0400

    set default image

---
+++ account/utils.py
@@ -0,0 +1,18 @@
+import os
+import random
+from django.conf import settings
+from django.core.files import File
+from django.core.files.storage import default_storage
+
+def get_random_lego_image_file():
+    static_lego_dir = os.path.join(settings.BASE_DIR, 'static/img/lego-icons/')
+    available_images = [
+        f for f in os.listdir(static_lego_dir)
+        if os.path.isfile(os.path.join(static_lego_dir, f)) and f.lower().endswith(('.png', '.jpg', '.jpeg'))
+    ]
+    if not available_images:
+        return None
+
+    chosen_file = random.choice(available_images)
+    full_path = os.path.join(static_lego_dir, chosen_file)
+    return full_path
