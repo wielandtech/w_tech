5046c9623c807531d4c7d5035003d34f0eca12be
Raphael Wieland <raphael@wielandtech.com>
2025-10-22 15:45:06 -0400

    Refactor blog templates to streamline content truncation
    Removed the custom 'truncate_with_read_more' tag and replaced it with direct usage of 'markdown' and 'truncatewords_html' in the blog templates. This change simplifies the code and enhances the display of truncated content, improving user navigation and overall content presentation in both the post list and search results.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,7 +3,6 @@ import markdown
 from django import template
 from django.db.models import Count
 from django.utils.safestring import mark_safe
-from django.utils.html import format_html
 
 from ..models import Post
 
@@ -13,37 +12,6 @@ register = template.Library()
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
-
-
-@register.simple_tag
-def truncate_with_read_more(text, word_count, post_url):
-    """
-    Truncate text and add 'Read More' link only if truncation occurred.
-    """
-    from django.template.defaultfilters import truncatewords_html
-    from django.utils.html import strip_tags
-    
-    # Process markdown first
-    processed_text = markdown.markdown(text)
-    
-    # Count words in the original text (without HTML tags)
-    original_word_count = len(strip_tags(processed_text).split())
-    
-    # If original text has fewer words than the limit, no truncation needed
-    if original_word_count <= word_count:
-        return mark_safe(processed_text)
-    
-    # Truncate the HTML content
-    truncated_html = truncatewords_html(processed_text, word_count)
-    
-    # Add the Read More link
-    read_more_link = format_html(
-        '{} <a href="{}" class="read-more-link">Read More</a>',
-        truncated_html,
-        post_url
-    )
-    
-    return read_more_link
 
 
 
