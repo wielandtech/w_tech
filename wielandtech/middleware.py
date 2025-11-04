f28b9b32e35ff0fa73b1a4b359205af9edbd3e17
Raphael Wieland <raphael@wielandtech.com>
2025-11-03 21:59:01 -0500

    Update SecurityHeadersMiddleware to include 'frame-src' directive for Google domains in Content Security Policy, enhancing security by allowing trusted frame sources.

--- wielandtech/middleware.py
+++ wielandtech/middleware.py
@@ -62,6 +62,7 @@ class SecurityHeadersMiddleware:
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
                 "img-src 'self' data: https:; "
                 "font-src 'self' data: https://fonts.gstatic.com; "
+                "frame-src 'self' https://www.google.com https://www.gstatic.com; "
                 "connect-src 'self' https://www.google-analytics.com https://cdn.jsdelivr.net "
                 "https://www.google.com https://www.gstatic.com;"
             )
